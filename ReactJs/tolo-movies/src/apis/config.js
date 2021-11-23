9619dc5237b150a32fa2ec9c7e512361ca2d9899
Marken <nctuan.ityu@gmail.com>
2021-11-23 21:03:41 +0000

    init

---
+++ ReactJs/tolo-movies/src/apis/config.js
@@ -0,0 +1,8 @@
+const configAPI = {
+    baseUrl: 'https://api.themoviedb.org/3/',
+    apiKey: 'ac004a0e39c2066a63a75eacce7367b8',
+    originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
+    w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
+}
+
+export default configAPI;
